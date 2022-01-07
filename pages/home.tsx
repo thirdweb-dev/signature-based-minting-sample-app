97fa93de2aa036006a20a7c8a20d4fffccd5c675
Ibrahim Ahmed <abeahmed2@gmail.com>
2022-01-07 14:32:22 -0800

    Creator/claimer buttons

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,12 +1,31 @@
-import { Flex, Heading } from "@chakra-ui/react";
+import { Button, Flex, Heading, Text } from "@chakra-ui/react";
 import useProtectedPage from "../hooks/useProtectedPage";
 
 export default function Home() {
   useProtectedPage();
 
   return (
-    <Flex>
-      <Heading>Creator Home</Heading>
+    <Flex flexDir="column" alignContent="center" textAlign="center">
+      <Heading size="lg">Home</Heading>
+
+      <Text>
+        If you're a creator, click the creator button. If you're a claimer,
+        click the claimer button.
+      </Text>
+
+      <Flex
+        flexDir="row"
+        sx={{
+          button: {
+            margin: "1rem",
+          },
+        }}
+        justifyContent="center"
+      >
+        <Button colorScheme="blue">Creator</Button>
+
+        <Button colorScheme="green">Claimer</Button>
+      </Flex>
     </Flex>
   );
 }
